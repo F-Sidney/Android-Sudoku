1aa8caf04faccd811fe1c91eb9c439a37037f180
Feng Sidney <nick.xf@gmail.com>
2012-03-14 00:14:32 +0800

    add About screen

--- src/com/nickwar/sudoku/Sudoku.java
+++ src/com/nickwar/sudoku/Sudoku.java
@@ -1,13 +1,39 @@
 package com.nickwar.sudoku;
 
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Sudoku extends Activity {
+public class Sudoku extends Activity implements OnClickListener {
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+        case R.id.about_button:
+            Intent i = new Intent(this,About.class);
+            startActivity(i);
+            break;
+
+        default:
+            break;
+        }
+    }
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        View continueButton = this.findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newButton = this.findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View aboutButton = this.findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = this.findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
     }
 }
